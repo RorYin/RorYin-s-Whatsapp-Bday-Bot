a62ef9498cc00256160563d8af3e4e94cdf82631
Dhanush N <71968114+RorYin@users.noreply.github.com>
2023-12-02 14:26:22 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
+import os
+
 # key = Fernet.generate_key()
 key = "Get a key by running above line, and use same here"
 
-airtablekey = "Airtable api key for ur account" #Depreciating on 1st Feb 2024,so better to use their new access token
+airtablekey = os.environ.get('airtablekey') #"Airtable api key for ur account" #Depreciating on 1st Feb 2024,so better to use their new access token
 
-TgBotToken = "5851812153:AAFqyJ5lgaxIrWJhC6HDCKIGzXN_ojncgVs"
+TgBotToken = os.environ.get('TgBotToken')
 
-devTGid = "887572477"  #will be used for later implementation of error log in Telegram group as telegram apis are very reliable.
+devTGid = os.environ.get('devTGid')  #will be used for later implementation of error log in Telegram group as telegram apis are very reliable.
 
-BaseId = "appwwKfKSUbG6gyDF"
+BaseId = os.environ.get('BaseId') #Change accordingly
 
-TableId = "tbl4u4JoL5tVlJ29L"
+TableId = os.environ.get('TableId') #Change accordingly
 
-TableName = "BDAY"
+TableName = os.environ.get('TableName')  #Change accordingly
 
-AirtableToken = "pate4umtgSBS5VSJe.b2073d7336f634033478e3e0d9bdf174dc9474f2c1050fe004b300a12b89a679"
+AirtableToken = os.environ.get('AirtableToken') #Change accordingly
 
 # greenapi instance id
-InstanceId = "7103881919"
+InstanceId = os.environ.get('InstanceId') #Change accordingly
 
 #greenapi api token instance
-ApiTokenInstance = "fc540920e4444202beb809deea4e0c32489714616ed646cda5"
+ApiTokenInstance = os.environ.get('ApiTokenInstance') #Change accordingly
 
 #deployed webapp url
-webappurl = "https://roryinswhatsappbdaybotwebapp.onrender.com/task"
+webappurl = os.environ.get('webappurl') #Change accordingly
