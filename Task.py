5f5195fc2107188890465db8619647be82b0dc96
Dhanush N <71968114+RorYin@users.noreply.github.com>
2023-12-02 16:44:31 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -2,9 +2,7 @@ import requests
 from setup import *
 
 def dotask():
-    url = webappurl #use your own deployed url
-    resp = requests.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
-    print(resp)
+    ToCheckIfAnyBdayToday()
 
 
 if __name__ == '__main__':
