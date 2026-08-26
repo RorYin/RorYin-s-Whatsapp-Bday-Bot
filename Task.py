3f17226805f00fda2458cde508766cc75fdbae78
Dhanush N <71968114+RorYin@users.noreply.github.com>
2026-08-26 20:56:03 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -3,8 +3,8 @@ import os
 from config import BASE_DIR
 from handler import check_work_anniversaries, checkbdays
 
-if os.path.isdir("/home/RorYinBot2/mysite"):
-    os.chdir("/home/RorYinBot2/mysite")
+if os.path.isdir("/home/RorYinBot2/Github/RorYin-s-Whatsapp-Bday-Bot"):
+    os.chdir("/home/RorYinBot2/Github/RorYin-s-Whatsapp-Bday-Bot")
 else:
     os.chdir(BASE_DIR)
 
